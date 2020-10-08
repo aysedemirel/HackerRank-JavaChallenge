3a638536689d82fa1f7ae3fa1f79a6377467bd02
Ayşe Demirel <aysedemirel1996@gmail.com>
2020-10-08 20:54:18 +0300

    Add string reverse challenge

---
+++ HackerRankJava/src/strings/StringReverse.java
@@ -0,0 +1,35 @@
+package strings;
+
+import java.util.Scanner;
+
+/**
+ * Given a string A, print Yes if it is a palindrome, print No otherwise.
+ * 
+ * @author aysedemirel
+ */
+public class StringReverse {
+  private static Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    String A = sc.next();
+    printResult(A);
+  }
+
+  private static void printResult(String A) {
+    if (A.equals(new String(getReverseString(A)))) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
+  }
+
+  private static byte[] getReverseString(String A) {
+    byte[] str = A.getBytes();
+    byte[] reverseStr = new byte[str.length];
+    int lastIndex = str.length - 1;
+    for (int i = lastIndex; i >= 0; i--) {
+      reverseStr[lastIndex - i] = str[i];
+    }
+    return reverseStr;
+  }
+}

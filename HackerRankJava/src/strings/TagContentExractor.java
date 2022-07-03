1fdf39e30028643b8163e97c17390faeb18951cb
aysedemirel <aysedemirel1996@gmail.com>
2022-07-03 15:36:46 +0300

    [REFC] Refactor

---
+++ HackerRankJava/src/strings/TagContentExractor.java
@@ -0,0 +1,18 @@
+package strings;
+
+import java.util.Scanner;
+
+public class TagContentExractor {
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int testCases = Integer.parseInt(in.nextLine());
+    while (testCases > 0) {
+      String line = in.nextLine();
+
+      // Write your code here
+
+      testCases--;
+    }
+  }
+}

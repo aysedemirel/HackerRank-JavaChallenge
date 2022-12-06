a8a892b5bfe19a39dc5470ea48efd3e61f926f3e
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2022-12-06 21:45:52 +0300

    Refactor inheritance 1

--- src/main/java/org/ayse/hackerrank/java/oop/Inheritance1.java
+++ src/main/java/org/ayse/hackerrank/java/oop/Inheritance1.java
@@ -2,32 +2,18 @@ package org.ayse.hackerrank.java.oop;
 
 /**
  * Print:
+ * <p>
  * I am walking
+ * <p>
  * I am flying
+ * <p>
  * I am singing
  *
- * @author aysedemirel
- * */
+ * @author aysedemireldeniz
+ */
 public class Inheritance1 {
-  class Animal{
-    void walk()
-    {
-      System.out.println("I am walking");
-    }
-  }
 
-  class Bird extends Animal
-  {
-    void fly()
-    {
-      System.out.println("I am flying");
-    }
-    void sing(){
-      System.out.println("I am singing");
-    }
-  }
-
-  public Inheritance1(){
+  public Inheritance1() {
     Bird bird = new Bird();
     bird.walk();
     bird.fly();
@@ -35,6 +21,24 @@ public class Inheritance1 {
   }
 
   public static void main(String[] args) {
-   new Inheritance1();
+    new Inheritance1();
+  }
+
+  class Animal {
+
+    void walk() {
+      System.out.println("I am walking");
+    }
+  }
+
+  class Bird extends Animal {
+
+    void fly() {
+      System.out.println("I am flying");
+    }
+
+    void sing() {
+      System.out.println("I am singing");
+    }
   }
 }

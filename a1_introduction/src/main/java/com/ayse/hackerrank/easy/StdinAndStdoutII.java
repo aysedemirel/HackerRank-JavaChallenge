f37be85665d1a66fd809f7b48283b3d1f77e4e7a
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2024-09-17 00:28:16 +0800

    Refactor StdinAndStdoutII

--- a1_introduction/src/main/java/com/ayse/hackerrank/easy/StdinAndStdoutII.java
+++ a1_introduction/src/main/java/com/ayse/hackerrank/easy/StdinAndStdoutII.java
@@ -11,19 +11,16 @@ import java.util.Scanner;
  */
 public class StdinAndStdoutII {
 
-  private static final Scanner scan = new Scanner(System.in);
-
-  public StdinAndStdoutII() {
-  }
-
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
     int i = scan.nextInt();
     double d = scan.nextDouble();
     scan.nextLine();
     String s = scan.nextLine();
+    scan.close();
     System.out.println("String: " + s);
     System.out.println("Double: " + d);
     System.out.println("Int: " + i);
-    scan.close();
   }
+  
 }

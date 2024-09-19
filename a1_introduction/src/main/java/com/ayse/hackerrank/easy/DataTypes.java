d471ef2fb7b60c44fa5c8ca632b7191c2bdb9f0c
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2024-09-19 23:21:58 +0800

    Refactor DataTypes

--- a1_introduction/src/main/java/com/ayse/hackerrank/easy/DataTypes.java
+++ a1_introduction/src/main/java/com/ayse/hackerrank/easy/DataTypes.java
@@ -1,62 +1,58 @@
 package com.ayse.hackerrank.easy;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * It is an example to find your input's type. </br> Type are byte, short, int, float, double, long
+ * It is an example to find your input's type. </br> Type are byte, short, int, long
  *
  * @author aysedemirel
  */
 public class DataTypes {
 
-  private static final Scanner scannerIn = new Scanner(System.in);
-  private final ArrayList<String> resultStr;
 
-  public DataTypes() {
-    int t = scannerIn.nextInt();
-    resultStr = new ArrayList<>();
-    getNumberFromUser(t);
-    printResult();
-  }
-
-  public static void main(String[] args) {
-    new DataTypes();
-  }
-
-  private void getNumberFromUser(int number) {
-    for (int i = 0; i < number; i++) {
-      try {
-        long x = scannerIn.nextLong();
-        resultStr.add(x + " can be fitted in:");
-        if (x >= -128 && x <= 127) {
-          resultStr.add("* byte");
-        }
-        if (x >= -32768 && x <= 32767) {
-          resultStr.add("* short");
-        }
-        if (x >= -2147483648 && x <= 2147483647) {
-          resultStr.add("* int");
-        }
-        if (x >= -9223372036854775808L && x <= 9223372036854775807L) {
-          resultStr.add("* long");
-        }
-        if (x <= 340282347 * Math.pow(10, 38) && x >= 140239846 * Math.pow(10, -45)) {
-          resultStr.add("* float");
-        }
-        if (x <= 17976931348623157L * Math.pow(10, 308)
-            && x >= 49406564584124654L * Math.pow(10, -324)) {
-          resultStr.add("* double");
-        }
-      } catch (Exception e) {
-        resultStr.add(scannerIn.next() + " can't be fitted anywhere.");
-      }
+    public static void main(String[] args) {
+        solutionOne();
     }
-  }
 
-  private void printResult() {
-    for (String result : resultStr) {
-      System.out.println(result);
+    public static void solutionOne() {
+        Scanner scannerIn = new Scanner(System.in);
+        int t = scannerIn.nextInt();
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = scannerIn.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= -128 && x <= 127)
+                    System.out.println("* byte");
+                if (x >= -32768 && x <= 32767)
+                    System.out.println("* short");
+                if (x >= -2147483648 && x <= 2147483647)
+                    System.out.println("* int");
+                if (x >= -9223372036854775808L && x <= 9223372036854775807L)
+                    System.out.println("* long");
+
+            } catch (Exception e) {
+                System.out.println(scannerIn.next() + " can't be fitted anywhere.");
+            }
+        }
     }
-  }
+
+    public static void solutionTwo() {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) System.out.println("* byte");
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) System.out.println("* short");
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) System.out.println("* int");
+                if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) System.out.println("* long");
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+
+        }
+    }
+
+
 }

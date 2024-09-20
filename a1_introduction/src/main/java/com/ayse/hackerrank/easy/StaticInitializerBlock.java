3a3c5608cb530a1ac3634605edfcef4e9294120f
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2024-09-21 00:18:46 +0800

    Refactor StaticInitializerBlock

--- a1_introduction/src/main/java/com/ayse/hackerrank/easy/StaticInitializerBlock.java
+++ a1_introduction/src/main/java/com/ayse/hackerrank/easy/StaticInitializerBlock.java
@@ -9,31 +9,32 @@ import java.util.Scanner;
  */
 public class StaticInitializerBlock {
 
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final int B;
-    private static final int H;
+    private static final String ERROR_MESSAGE = "Breadth and height must be positive";
+    private static int breadth;
+    private static int height;
     private static boolean flag = false;
 
     static {
-        B = scanner.nextInt();
-        H = scanner.nextInt();
-        try {
-            if (B <= 0 || H <= 0) {
-                throw new Exception("Breadth and height must be positive");
-            } else {
-                flag = true;
+        initializeDimensions();
+    }
+
+    private static void initializeDimensions() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            breadth = scanner.nextInt();
+            height = scanner.nextInt();
+            if (breadth <= 0 || height <= 0) {
+                throw new IllegalArgumentException(ERROR_MESSAGE);
             }
+            flag = true;
         } catch (Exception e) {
-            System.out.println(e);
-            flag = false;
+            System.out.println(e.getMessage());
         }
     }
 
-
     public static void main(String[] args) {
         if (flag) {
-            int area = B * H;
+            int area = breadth * height;
             System.out.print(area);
         }
-    }// end of main
+    }
 }

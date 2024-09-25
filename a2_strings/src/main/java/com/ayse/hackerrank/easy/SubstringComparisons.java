101be9178f3f700241528c789e383b1c8d1088f1
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2024-09-26 00:30:37 +0800

    Refactor SubstringComparisons

--- a2_strings/src/main/java/com/ayse/hackerrank/easy/SubstringComparisons.java
+++ a2_strings/src/main/java/com/ayse/hackerrank/easy/SubstringComparisons.java
@@ -1,6 +1,6 @@
 package com.ayse.hackerrank.easy;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Return the respective lexicographically smallest and largest substrings as a single
@@ -10,20 +10,53 @@ import java.util.Scanner;
  */
 public class SubstringComparisons {
 
-    public static String getSmallestAndLargest(String text, int seperatedIndex) {
-        int startIndex = 0;
-        String tempText = text.substring(startIndex, seperatedIndex);
-        String smallest = tempText;
-        String largest = tempText;
-        for (int i = seperatedIndex; i < text.length(); i++) {
-            tempText = tempText.substring(startIndex + 1, seperatedIndex) + text.charAt(i);
-            if (largest.compareTo(tempText) < 0) {
-                largest = tempText;
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        for (int i = 0; i <= s.length() - k; i++) {
+            String subStr = s.substring(i, i + k);
+            if (subStr.compareTo(smallest) < 0 || smallest.isEmpty()) {
+                smallest = subStr;
             }
-            if (smallest.compareTo(tempText) > 0) {
-                smallest = tempText;
+            if (subStr.compareTo(largest) > 0 || largest.isEmpty()) {
+                largest = subStr;
             }
         }
+        return smallest + "\n" + largest;
+    }
+
+    public static String getSmallestAndLargestList(String s, int k) {
+        List<String> substrings = new ArrayList<>();
+        for (int i = 0; i <= s.length() - k; i++) {
+            // Add a substring of length 'k' to our list
+            substrings.add(s.substring(i, i + k));
+        }
+        // Sort list
+        Collections.sort(substrings);
+
+        // Set first and last elements as smallest and largest
+        String smallest = substrings.get(0);
+        String largest = substrings.get(substrings.size() - 1);
+
+        return smallest + "\n" + largest;
+    }
+
+    public static String getSmallestAndLargestArray(String s, int k) {
+        // Because each substring has length 'k', we know the number of possible substrings
+        String[] substrings = new String[s.length() - k + 1];
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            // Add a substring of length 'k' to our array
+            substrings[i] = s.substring(i, i + k);
+        }
+
+        // Sort array
+        Arrays.sort(substrings);
+
+        // Set first and last elements as smallest and largest
+        String smallest = substrings[0];
+        String largest = substrings[substrings.length - 1];
+
         return smallest + "\n" + largest;
     }
 
